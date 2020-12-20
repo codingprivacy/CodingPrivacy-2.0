6fdc67c1f41f291fd1955158088d35415b994f4e
Harsh Patel <hr402607@dal.ca>
2020-12-20 00:15:55 -0400

    CW-13 create starter code for react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const React = require('react');
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is CodingPrivacy website, deployed on Github Pages.</h1>
+    </div>
+  );
+}
+
+export default App;
