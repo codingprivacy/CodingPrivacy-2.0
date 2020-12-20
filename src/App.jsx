7249293c015e8521c98a449ac866b36683e0313e
Harsh Patel <harshgp44@gmail.com>
2020-12-19 23:32:47 -0400

    Added Starter code for the project, created Github Workflow.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const React = require('react');
+
+const App = () => (
+  <div className="App">
+    <h1>This is CodingPrivacy website, This is how.</h1>
+  </div>
+);
+
+export default App;
