734001f0fcf14e99919491219fbc06544ed8d87c
Harsh Patel <harshgp44@gmail.com>
2020-12-20 22:47:50 -0400

    Develop (#6)
    * CW-13 create starter code for react
    
    * Github workflow changed
    
    * Rebased and changed contents

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const React = require('react');
+
+const App = () => (
+  <div className="App">
+    <h1>This is CodingPrivacy website, deployed on Github Pages.</h1>
+  </div>
+);
+
+export default App;
