9250fc2e3c3e178c68f1ca9395cc53dd963dcf64
Harsh Patel <harshgp44@gmail.com>
2021-01-02 19:06:36 -0400

    CW-22 Searchbar complete (#18)
    Merging Search bar to develop

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.scss';
 import { Container, Row, Col } from 'react-bootstrap';
 import Feeds from './apps/homepage/components/feeds/Feeds';
 import Tags from './apps/homepage/components/Tags';
+import SearchBar from './apps/homepage/components/SearchBar';
 
 const React = require('react');
 
 const App = () => (
   <div className="App">
     <Container fluid className="appContainer">
+      <Row>
+        <SearchBar />
+      </Row>
       <Row>
         <Col className=".bg-transparent" />
         <Col lg={6}>
